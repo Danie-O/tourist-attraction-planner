df37266f1747604e43ccb87d8d15f64e729c1f33
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-07-06 00:28:40 +0000

    Fix bugs from form implementation

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from locations import Locations
 from forms import AddLocationForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRET_PROJECT' # define secret key
+app.config['SECRET_KEY'] = 'SECRET_PROJECT'
 
 visit = Locations()
 categories = {"recommended": "Recommended", "tovisit": "Places To Go", "visited": "Visited!!!", }
@@ -13,34 +13,33 @@ DEL_ACTION = "X"
 
 @app.route("/<category>", methods=["GET", "POST"])
 def locations(category):
-  locations = visit.get_list_by_category(category)
-  ## Check the request for form data and process
-  if request.method == "POST":
-    [(name, action)] = [(None, None)]
+    locations = visit.get_list_by_category(category)
+    ## Check the request for form data and process
+    if request.method == "POST":
+        [(name, action)] = request.form.items()
 
-    if action == UP_ACTION:
-      visit.moveup(name)
-    elif action == DEL_ACTION:
-      visit.delete(name)
-
-  return render_template('locations.html', template_category=category, template_categories=categories, template_locations=locations, add_location=AddLocationForm())
+        if action == UP_ACTION:
+            visit.moveup(name)
+        elif action == DEL_ACTION:
+            visit.delete(name)
+    return render_template('locations.html', template_category=category, template_categories=categories, template_locations=locations, add_location=AddLocationForm())
 
 @app.route("/add_location", methods=["POST"])
 def add_location():
-  ## Validate and collect the form data
-  add_form = AddLocationForm(csrf_enabled=False)
+    add_location = AddLocationForm()
 
-  if add_form.validate_on_submit():
-      name = add_form.name.data
-      description = add_form.description.data
-      category = add_form.category.data
-      visit.add(name, description, category)
+    if add_location.validate_on_submit:
+        name = add_location.name.data
+        description  = add_location.description.data
+        category = add_location.category.data
+        visit.add(name, description, category)
+    # else:
+        # category = "recommended"
 
-  ## Redirect to locations route function
-  return redirect(url_for("locations", category=category, _external=True, _scheme='https'))
+    ## Redirect to locations route function
+    return redirect(url_for("locations", category=category, _external=True, _scheme='https'))
 
 @app.route("/")
 def index():
-
-  ## Redirect to locations route function
-  return redirect(url_for("location", category="recommended", _external=True, _scheme='https'))
+    ## Redirect to locations route function
+    return redirect(url_for("locations", category="recommended", _external=True, _schema='https'))
